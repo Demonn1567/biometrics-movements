2f3d2277e54e389dc58913c3293565e6d4f26b5c
Demonn1567 <krishsharmacool3@gmail.com>
2024-08-22 18:26:00 +0530

    render-update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import UserBehaviorTracker from './UserBehaviorTracker';
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     return (
         <div className="App">
-          <Analytics/>
             <UserBehaviorTracker />
         </div>
     );
